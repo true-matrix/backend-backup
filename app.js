ef8b5926a8b9c238970f24b896dd3c97af7ff0f2
True Matrix <rajesh.truematrix@gmail.com>
2023-12-05 13:15:17 +0530

    update to 3000 port

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ const app = express();
 app.use(
   cors({
     // origin: "*",
-    // origin: 'http://localhost:3000',
-    origin: 'https://wolf.blutrain.net',
+    origin: 'http://localhost:3000',
+    // origin: 'https://wolf.blutrain.net',
 
     methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
 
@@ -98,14 +98,14 @@ app.use(
   })
 ); // Returns middleware that only parses urlencoded bodies
 
-app.use('/uploads', express.static('uploads'))
-// app.use('/uploads', express.static('uploads'), (req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://wolf.blutrain.net'); // Specify your client's domain
-// // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
-// // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-// // res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-//   next();
-// });
+// app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static('uploads'), (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://backend-api-0pbl.onrender.com'); // Specify your client's domain
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+  next();
+});
 app.use(mongosanitize());
 
 app.use(xss());
