667fe692d640b9f320e9c85dfd1aa0e90df79125
True Matrix <rajesh.truematrix@gmail.com>
2023-12-05 13:19:51 +0530

    update

--- app.js
+++ app.js
@@ -101,9 +101,9 @@ app.use(
 // app.use('/uploads', express.static('uploads'))
 app.use('/uploads', express.static('uploads'), (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://backend-api-0pbl.onrender.com'); // Specify your client's domain
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
+// // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// // res.setHeader('Access-Control-Allow-Credentials', 'true'); 
   next();
 });
 app.use(mongosanitize());
