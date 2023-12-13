f8c2c3e2d42e512020e399418069184a81d5c688
True Matrix <rajesh.truematrix@gmail.com>
2023-12-13 13:27:02 +0530

    new conversation api

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -22,46 +22,72 @@ const catchAsync = require("../utils/catchAsync");
 //     }
 //   });
 
+// exports.newConversation = catchAsync(async (req, res, next) => {
+//   const senderId = req.body.senderId;
+//   const receiverId = req.body.receiverId;
+
+//   // Check if a conversation already exists
+//   const existingConversation = await Conversation.findOne({
+//     members: {
+//       $all: [senderId, receiverId],
+//     },
+//   });
+
+//   if (existingConversation) {
+//     // Redirect to the existing conversation
+//     return res.status(200).json({
+//       status: 'success',
+//       data: existingConversation,
+//       message: 'Existing conversation found',
+//     });
+//   }
+
+//   // If no existing conversation, create a new one
+//   const newConversation = new Conversation({
+//     members: [senderId, receiverId],
+//   });
+
+//   try {
+//     const savedConversation = await newConversation.save();
+//     res.status(200).json({
+//       status: 'success',
+//       data: savedConversation,
+//       message: 'New conversation started',
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       status: 'error',
+//       message: 'Internal server error',
+//     });
+//   }
+// });
+
 exports.newConversation = catchAsync(async (req, res, next) => {
   const senderId = req.body.senderId;
   const receiverId = req.body.receiverId;
 
-  // Check if a conversation already exists
-  const existingConversation = await Conversation.findOne({
-    members: {
-      $all: [senderId, receiverId],
+  // Use findOneAndUpdate with upsert option
+  const existingConversation = await Conversation.findOneAndUpdate(
+    {
+      members: {
+        $all: [senderId, receiverId],
+      },
     },
+    {}, // empty update, no actual changes to the document
+    {
+      upsert: true,
+      new: true, // return the modified document
+    }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: existingConversation,
+    message: existingConversation ? 'Existing conversation found' : 'New conversation started',
   });
-
-  if (existingConversation) {
-    // Redirect to the existing conversation
-    return res.status(200).json({
-      status: 'success',
-      data: existingConversation,
-      message: 'Existing conversation found',
-    });
-  }
-
-  // If no existing conversation, create a new one
-  const newConversation = new Conversation({
-    members: [senderId, receiverId],
-  });
-
-  try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json({
-      status: 'success',
-      data: savedConversation,
-      message: 'New conversation started',
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  }
 });
+
 
 
 // Get Conversation of a user
