d06edcd16ed2ae4cb22009b46bada53f86710580
True Matrix <rajesh.truematrix@gmail.com>
2024-01-18 15:30:00 +0530

    userRole added

--- models/user.js
+++ models/user.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
   gender: {
     type: String,
   },
+  userRole: {
+    type: String,
+    default: "alpha",
+  },
   email: {
     type: String,
     required: [true, "Email is required"],
