a68cb565016b53d94e94f0115788dac528b0bddc
True Matrix <rajesh.truematrix@gmail.com>
2024-02-19 15:34:10 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -376,7 +376,7 @@ exports.sendOTP = catchAsync(async (req, res, next) => {
     upperCaseAlphabets: false
   });
 
-  const otp_expiry_time = Date.now() + 10 * 60 * 1000; // otp validation : 10 Mins after otp is sent
+  const otp_expiry_time = Date.now() + 2 * 60 * 1000; // otp validation : 2 Mins after otp is sent
 
   const user = await User.findByIdAndUpdate(userId, {
     otp_expiry_time: otp_expiry_time,
